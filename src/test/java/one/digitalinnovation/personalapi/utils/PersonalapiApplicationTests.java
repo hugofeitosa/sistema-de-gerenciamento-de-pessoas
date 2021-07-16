f3d0766142394a02e1cce0c3c888586b5c9de80d
hugofeitosa <hugofeitosa93@gmail.com>
2021-07-16 12:09:08 -0300

    final version

---
+++ src/test/java/one/digitalinnovation/personalapi/utils/PersonalapiApplicationTests.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.personalapi.utils;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
